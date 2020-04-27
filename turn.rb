d801b38f58a6bea370a0f302efca6c97b998b947
aliyasser20 <aliyasser20@hotmail.com>
2020-04-27 14:07:19 -0400

    update class turn

--- turn.rb
+++ turn.rb
@@ -1,17 +1,12 @@
 # frozen_string_literal: true
 
 class Turn
-  def initialize
-    # @play = play
-    @current_player_id = 1
-  end
-
-  def play_turn(question)
-    puts "Player #{@current_player_id}: #{question}"
+  def play_turn(current_player_id, question)
+    puts "Player #{current_player_id}: #{question}"
     input = gets.chomp
     puts input
   end
 end
 
 turn_one = Turn.new
-turn_one.play_turn('hello')
+turn_one.play_turn(3, 'hello')
